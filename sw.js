1b971bbe4dc72d377ccc8ea71dcef8907f0742b1
robmanihuruk-srgh <robmanihuruk@gmail.com>
2026-04-11 12:57:22 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,18 @@
-const CACHE_NAME = 'notula-cache-v2';
-const urlsToCache = ['./', './index.html', './icon.jpg', './manifest.json'];
+const CACHE_NAME = 'notula-cache-v3';
+const urlsToCache = ['/', '/index.html', '/icon.jpg', '/manifest.json'];
 
 self.addEventListener('install', event => {
-  self.skipWaiting(); // Memaksa browser langsung memakai versi terbaru
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-      .catch(err => console.log('Cache gagal, tapi SW tetap jalan:', err))
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request).catch(() => console.log('Fetch offline'));
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(cacheNames => Promise.all(
+    cacheNames.map(cache => {
+      if (cache !== CACHE_NAME) return caches.delete(cache);
     })
-  );
+  )));
 });
